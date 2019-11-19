e720231ee54d42686f2b59f3220f9cbe35cc7343
Gianluca Recchia <gianluca.recchia@protonmail.com>
2019-11-19 03:45:34 +0100

    Add pre-save hook for deleting trailing whitespaces

--- hooks.el
+++ hooks.el
@@ -1,3 +1,7 @@
+(add-hook 'before-save-hook
+          (lambda()
+            (delete-trailing-whitespace)))
+
 (add-hook 'emacs-lisp-mode-hook
           (lambda()
             (setq indent-tabs-mode nil)))
