50984b4e84807b44e6bc467a0b50873f6785873a
Gianluca Recchia <gianluca.recchia@protonmail.com>
2019-11-19 02:57:07 +0100

    Make ibuffer appear in the current window

--- shortcuts.el
+++ shortcuts.el
@@ -4,6 +4,6 @@
 (global-set-key (kbd "C-c p") 'project-find-file)
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 (global-set-key (kbd "C-x C-S-s") 'save-some-buffers)
-(global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-<next>") 'next-buffer)
 (global-set-key (kbd "M-<prior>") 'previous-buffer)
