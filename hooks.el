baa786226b9673ad506fa7acfb81a017b620a324
Gianluca Recchia <gianluca.recchia@protonmail.com>
2019-12-16 02:56:29 +0100

    Fix keybinding issues
    Keybindings were not being assigned correctly by use-package.
    Configuration of the keybindings went through a general cleanup and
    improvement in its structure, following use-package's syntax.

--- hooks.el
+++ hooks.el
@@ -8,8 +8,3 @@
 
 (add-hook 'rust-mode-hook
           (lambda () (setq indent-tabs-mode nil)))
-
-(add-hook 'company-mode-hook (lambda ()
-                               (substitute-key-definition 'company-complete-common
-                                                          'company-yasnippet-or-completion
-                                                          company-active-map)))
--- packages.el
+++ packages.el
@@ -22,48 +22,53 @@
   (("C-x g" . magit-status)))
 
 (use-package company
+  :demand
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-selection))
   :custom
   (company-idle-delay 0.2)
   (company-minimum-prefix-length 2)
   :config
-  (global-set-key (kbd "C-<tab>") 'company-complete)
-  (define-key company-active-map (kbd "M-p") (lambda () (interactive) (company-complete-common-or-cycle -1)))
-  (define-key company-active-map (kbd "M-n") (lambda () (interactive) (company-complete-common-or-cycle 1)))
-  (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
   (global-company-mode))
 
 (use-package yasnippet
-  :config (yas-reload-all)
-  :hook (prog-mode . yas-minor-mode))
+  :demand
+  :bind (:map yas-minor-mode-map
+              ("TAB" . nil)
+              ("<tab>" . nil)
+              ("<C-tab>" . yas-expand))
+  :config
+  (yas-global-mode 1))
 
 (use-package ivy
+  :demand
   :custom
   (ivy-use-virtual-buffers t)
   (ivy-count-format "(%d/%d) ")
   (ivy-wrap t)
+  :bind (("C-x b" . ivy-switch-buffer)
+         ("C-c v" . ivy-push-view)
+         ("C-c V" . ivy-pop-view))
   :config
-  (global-key-binding (kbd "C-x b") 'ivy-switch-buffer)
-  (global-key-binding (kbd "C-c v") 'ivy-push-view)
-  (global-key-binding (kbd "C-c V") 'ivy-pop-view)
   (ivy-mode 1))
 
 (use-package swiper
   :after ivy
-  :config
-  (global-key-binding (kbd "C-s") 'swiper-isearch))
+  :demand
+  :bind ("C-s" . swiper-isearch))
 
 (use-package counsel
   :after swiper
-  :config
-  (global-key-binding (kbd "M-x") 'counsel-M-x)
-  (global-key-binding (kbd "C-x C-f") 'counsel-find-file)
-  (global-key-binding (kbd "M-y") 'counsel-yank-pop)
-  (global-key-binding (kbd "<f1> f") 'counsel-describe-function)
-  (global-key-binding (kbd "<f1> v") 'counsel-describe-variable)
-  (global-key-binding (kbd "<f1> l") 'counsel-find-library)
-  (global-key-binding (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  (global-key-binding (kbd "<f2> u") 'counsel-unicode-char)
-  (global-key-binding (kbd "<f2> j") 'counsel-set-variable))
+  :demand
+  :bind (("M-x". counsel-M-x)
+         ("C-x C-f". counsel-find-file)
+         ("M-y". counsel-yank-pop)
+         ("<f1> f". counsel-describe-function)
+         ("<f1> v". counsel-describe-variable)
+         ("<f1> l". counsel-find-library)
+         ("<f2> i". counsel-info-lookup-symbol)
+         ("<f2> u". counsel-unicode-char)
+         ("<f2> j". counsel-set-variable)))
 
 (use-package multiple-cursors
   :config
