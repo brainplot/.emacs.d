d7ea9fb323a32d1425f250a93bd790b97ddd4ccd
Gianluca Recchia <gianluca.recchia@protonmail.com>
2019-12-23 06:07:28 +0100

    Clean up formatting

--- hooks.el
+++ hooks.el
@@ -7,4 +7,5 @@
             (setq indent-tabs-mode nil)))
 
 (add-hook 'rust-mode-hook
-          (lambda () (setq indent-tabs-mode nil)))
+          (lambda ()
+            (setq indent-tabs-mode nil)))
