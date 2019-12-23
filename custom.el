f48caddbbe3926e02ab5714de980cc5bef24cb20
Gianluca Recchia <gianluca.recchia@protonmail.com>
2019-12-23 02:12:18 +0100

    Remove useless setting -- Again!
    For some reason, first time didn't work.

--- custom.el
+++ custom.el
@@ -5,10 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (emmet-mode web-mode pinentry markdown-mode expand-region ace-window multiple-cursors counsel modus-vivendi-theme yasnippet-snippets yasnippet rust-mode fish-mode magit cmake-mode company eglot rainbow-delimiters use-package)))
- '(safe-local-variable-values
-   (quote
-    ((eval setenv "CLANGD_FLAGS" "--compile-commands-dir=build")))))
+    (emmet-mode web-mode pinentry markdown-mode expand-region ace-window multiple-cursors counsel modus-vivendi-theme yasnippet-snippets yasnippet rust-mode fish-mode magit cmake-mode company eglot rainbow-delimiters use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
