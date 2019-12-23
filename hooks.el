1d88ea9e11a138e870666016b799f87ed67c3053
Gianluca Recchia <gianluca.recchia@protonmail.com>
2019-12-23 09:31:30 +0100

    Indent with spaces when working with web languages

--- hooks.el
+++ hooks.el
@@ -9,3 +9,19 @@
 (add-hook 'rust-mode-hook
           (lambda ()
             (setq indent-tabs-mode nil)))
+
+(add-hook 'sgml-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)))
+
+(add-hook 'css-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)))
+
+(add-hook 'js-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)))
+
+(add-hook 'js-jsx-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)))
