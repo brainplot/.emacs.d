e7d17fcec19ba9349f6adef8c6ab7fc9a2edf520
Gianluca Recchia <gianluca.recchia@protonmail.com>
2020-01-02 06:22:00 +0100

    Delete useless comment
    The comment was left there because I was trying out Emacs' default
    scrolling behavior on Linux as well. I'm getting used to it and
    finding it quite useable so I think this comment is no longer needed.

--- init.el
+++ init.el
@@ -6,9 +6,6 @@
       tab-always-indent 'complete
       use-package-always-ensure t)
 
-;; (unless (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
-;;   (setq scroll-conservatively most-positive-fixnum))
-
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
 (load custom-file)
