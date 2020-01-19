e5b411c6593de6303e60e5237f1eea13ad9d1b36
Gianluca Recchia <gianluca.recchia@protonmail.com>
2020-01-19 12:27:16 +0100

    Move snippet in its own scope
    This patch moves the block that cycles through the modes which use
    spaces for indentation in its own scope. That way the indent-spaces-list
    is not exposed, since there's no need to.

--- lisp/hooks.el
+++ lisp/hooks.el
@@ -1,6 +1,5 @@
 (add-hook 'before-save-hook #'clean-up-buffer)
 
-(setq indent-spaces-list '(emacs-lisp sgml css js js-jsx))
-
-(dolist (mode indent-spaces-list)
-  (add-hook (intern (concat (symbol-name mode) "-mode-hook")) #'indent-with-spaces))
+(let ((indent-spaces-list '(emacs-lisp sgml css js js-jsx)))
+  (dolist (mode indent-spaces-list)
+    (add-hook (intern (concat (symbol-name mode) "-mode-hook")) #'indent-with-spaces)))
