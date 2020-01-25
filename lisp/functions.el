564624c7447ec408b8f07de8ceebe03c11406a87
Gianluca Recchia <gianluca.recchia@protonmail.com>
2020-01-25 15:07:24 +0100

    Add a final period to functions' docstring

--- lisp/functions.el
+++ lisp/functions.el
@@ -1,12 +1,12 @@
 (defun open-previous-line (arg)
-  "Open a new line before the current one"
+  "Open a new line before the current one."
   (interactive "p")
   (beginning-of-line)
   (open-line arg)
   (indent-according-to-mode))
 
 (defun open-next-line (arg)
-  "Move to the next line and then opens a line"
+  "Move to the next line and then opens a line."
   (interactive "p")
   (end-of-line)
   (open-line arg)
@@ -14,11 +14,11 @@
   (indent-according-to-mode))
 
 (defun clean-up-buffer ()
-  "Perform some clean up operations in the current buffer"
+  "Perform some clean up operations in the current buffer."
   (if (and (fboundp #'eglot-current-server) (eglot-current-server))
       (eglot-format-buffer)
     (whitespace-cleanup)))
 
 (defun indent-with-spaces ()
-  "Set indentation for the current buffer to spaces"
+  "Set indentation for the current buffer to spaces."
   (setq indent-tabs-mode nil))
