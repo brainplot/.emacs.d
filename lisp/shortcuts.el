e696c00fa93d0425012157fdb81e8b6144344018
Gianluca Recchia <gianluca.recchia@protonmail.com>
2020-02-19 07:56:31 +0100

    Change keybinding for whitespace-mode

--- lisp/shortcuts.el
+++ lisp/shortcuts.el
@@ -3,7 +3,7 @@
 (global-set-key (kbd "C-c /") #'comment-or-uncomment-region)
 (global-set-key (kbd "C-c C-k") #'kill-some-buffers)
 (global-set-key (kbd "C-c C-r") #'replace-string)
-(global-set-key (kbd "C-c C-w") #'whitespace-mode)
+(global-set-key (kbd "C-c m w") #'whitespace-mode)
 (global-set-key (kbd "C-c a") #'copy-from-above-command)
 (global-set-key (kbd "C-c p") #'project-find-file)
 (global-set-key (kbd "C-c r") #'revert-buffer)
