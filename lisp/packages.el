96eb664afb2fdbff6247d128ed46131ed9c6b96e
Gianluca Recchia <gianluca.recchia@protonmail.com>
2020-03-27 07:48:10 +0100

    Switch to melpa stable as the package repository

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,5 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (when (eq system-type 'windows-nt)
