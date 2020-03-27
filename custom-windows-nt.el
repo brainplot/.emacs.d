417066673b6a5a035d6fa8430ae5a2b3bbcf91e6
Gianluca Recchia <gianluca.recchia@protonmail.com>
2020-03-27 08:58:42 +0100

    Delete yasnippet-snippets on Windows

--- custom-windows-nt.el
+++ custom-windows-nt.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dired-toggle modus-vivendi-theme yasnippet-snippets yasnippet yaml-mode typescript-mode rust-mode rainbow-delimiters powershell multiple-cursors move-text markdown-mode magit js2-mode google-this gitignore-mode gitconfig-mode gitattributes-mode expand-region emmet-mode eglot diredfl ace-window ninja-mode counsel company use-package))))
+    (dired-toggle modus-vivendi-theme yasnippet yaml-mode typescript-mode rust-mode rainbow-delimiters powershell multiple-cursors move-text markdown-mode magit js2-mode google-this gitignore-mode gitconfig-mode gitattributes-mode expand-region emmet-mode eglot diredfl ace-window ninja-mode counsel company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
